603d33e2d2f085dc0ce9526d2629f30cd43cc84d
imranq2 <imranq2@hotmail.com>
2020-11-02 15:09:08 -0800

    get test passing

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Question(models.Model):
+    name = models.CharField(max_length=20)
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from polls.models import Question
 
-# Create your tests here.
+
+class QuestionTestCase(TestCase):
+    def setUp(self):
+        Question.objects.create(name="1", question_text="What is your name?", pub_date="2020-01-01")
+        Question.objects.create(name="2", question_text="Where do you live?", pub_date="2020-01-01")
+
+    def test_questions_can_speak(self):
+        first = Question.objects.get(name="1")
+        second = Question.objects.get(name="2")
+        self.assertEqual(first.question_text, 'What is your name?')
+        self.assertEqual(second.question_text, 'Where do you live?')
